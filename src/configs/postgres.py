b4ab7ad292187bbaad467628b7fee9c3c6ec20bd
Yadnyesh77 <120787736+Yadnyesh77@users.noreply.github.com>
2025-03-28 10:40:09 +0530

    weather:Connected with neon db

--- src/configs/postgres.py
+++ src/configs/postgres.py
@@ -1,22 +1,25 @@
-from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
-username = "postgres"
-password = quote_plus("Yadnyesh@7841")
-host = "localhost"  # or "::1" for IPv6
-port = "5432"
-database = "postgres"
+# username = "postgres"
+# password = quote_plus("Yadnyesh@7841")
+# host = "localhost"  # or "::1" for IPv6
+# port = "5432"
+# database = "postgres"
 
 # SQLAlchemy connection URL
-DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+DATABASE_URL = os.getenv("Database_url")
 
 # Create the PostgreSQL engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,pool_pre_ping=True)
 
 # Create a session factory
-SessionLocal = sessionmaker( bind=engine)
+SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
 
 # Dependency to get the database session
 def get_db():
